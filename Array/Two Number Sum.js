5ed00ba2c5888102a501953d8948415b59938867
Manraj <manrajkhokhar1995@gmail.com>
2020-04-30 14:37:47 -0700

    add two sum array problem

---
+++ Array/Two Number Sum.js
@@ -0,0 +1,12 @@
+function twoNumberSum(array, targetSum) {
+    // Write your code here.
+    for (let i = 0; i < array.length; i++) {
+        for (let j = i + 1; j < array.length; j++) {
+            if ((array[i] + array[j]) === targetSum) return [array[i], array[j]];
+        }
+    }
+    return []
+}
+
+// Do not edit the line below.
+exports.twoNumberSum = twoNumberSum;
